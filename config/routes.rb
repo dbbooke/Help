e899a55d5f452d52c1220bf29b464f9e994acb90
Darren Booker <darrenbooker@Darrens-MacBook-Pro.local>
2018-03-10 11:10:12 -0700

    New update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  get 'contacts/create'
-  resources :contacts, only: [:new, :create]
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
